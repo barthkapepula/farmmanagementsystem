baae670e7ecc7455b72592af618686ff5457a000
Bartholomew <65113211+barthkapepula@users.noreply.github.com>
2024-02-13 09:28:15 +0200

    Add files via upload

---
+++ shopingcart/index.php
@@ -0,0 +1,20 @@
+<?php
+function pdo_connect_mysql() {
+    $DATABASE_HOST = 'localhost';
+    $DATABASE_USER = 'root';
+    $DATABASE_PASS = '';
+    $DATABASE_NAME = 'fms';
+    try {
+        return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
+    } catch (PDOException $exception) {
+        exit('Failed to connect to database!');
+    }
+}
+// Include functions and connect to the database using PDO MySQL
+include 'functions.php';
+$pdo = pdo_connect_mysql();
+// Page is set to home (home.php) by default, so when the visitor visits, that will be the page they see.
+$page = isset($_GET['page']) && file_exists($_GET['page'] . '.php') ? $_GET['page'] : 'home';
+// Include and show the requested page
+include $page . '.php';
+?>
---
+++ shopingcart/products.php
@@ -0,0 +1,99 @@
+<?php
+// Check to make sure the id parameter is specified in the URL
+if (isset($_GET['id'])) {
+    // Prepare statement and execute, prevents SQL injection
+    $stmt = $pdo->prepare('SELECT * FROM products WHERE id = ?');
+    $stmt->execute([$_GET['id']]);
+    // Fetch the product from the database and return the result as an Array
+    $product = $stmt->fetch(PDO::FETCH_ASSOC);
+    // Check if the product exists (array is not empty)
+    if (!$product) {
+        // Simple error to display if the id for the product doesn't exists (array is empty)
+        exit('Product does not exist!');
+    }
+} else {
+    // Simple error to display if the id wasn't specified
+    exit('Product does not exist!');
+}
+?>
+<?=template_header('Product')?>
+
+<div class="product content-wrapper">
+    <img src="imgs/<?=$product['img']?>" width="500" height="500" alt="<?=$product['name']?>">
+    <div>
+        <h1 class="name"><?=$product['name']?></h1>
+        <span class="price">
+            &dollar;<?=$product['price']?>
+            <?php if ($product['rrp'] > 0): ?>
+            <span class="rrp">&dollar;<?=$product['rrp']?></span>
+            <?php endif; ?>
+        </span>
+        <form action="index.php?page=cart" method="post">
+            <input type="number" name="quantity" value="1" min="1" max="<?=$product['quantity']?>" placeholder="Quantity" required>
+            <input type="hidden" name="product_id" value="<?=$product['id']?>">
+            <input type="submit" value="Add To Cart">
+        </form>
+        <div class="description">
+            <?=$product['desc']?>
+        </div>
+    </div>
+</div>
+
+<?=template_footer()?>
+<?=template_header('Home')?>
+
+<div class="featured">
+    <h2>Gadgets</h2>
+    <p>Essential gadgets for everyday use</p>
+</div>
+<div class="recentlyadded content-wrapper">
+    <h2>Recently Added Products</h2>
+    <div class="products">
+        <?php foreach ($recently_added_products as $product): ?>
+        <a href="index.php?page=product&id=<?=$product['id']?>" class="product">
+            <img src="imgs/<?=$product['img']?>" width="200" height="200" alt="<?=$product['name']?>">
+            <span class="name"><?=$product['name']?></span>
+            <span class="price">
+                &dollar;<?=$product['price']?>
+                <?php if ($product['rrp'] > 0): ?>
+                <span class="rrp">&dollar;<?=$product['rrp']?></span>
+                <?php endif; ?>
+            </span>
+        </a>
+        <?php endforeach; ?>
+    </div>
+</div>
+// Get the total number of products
+$total_products = $pdo->query('SELECT * FROM products')->rowCount();
+?>
+<?=template_footer()?>
+<?=template_header('Products')?>
+
+<div class="products content-wrapper">
+    <h1>Products</h1>
+    <p><?=$total_products?> Products</p>
+    <div class="products-wrapper">
+        <?php foreach ($products as $product): ?>
+        <a href="index.php?page=product&id=<?=$product['id']?>" class="product">
+            <img src="imgs/<?=$product['img']?>" width="200" height="200" alt="<?=$product['name']?>">
+            <span class="name"><?=$product['name']?></span>
+            <span class="price">
+                &dollar;<?=$product['price']?>
+                <?php if ($product['rrp'] > 0): ?>
+                <span class="rrp">&dollar;<?=$product['rrp']?></span>
+                <?php endif; ?>
+            </span>
+        </a>
+        <?php endforeach; ?>
+    </div>
+    <div class="buttons">
+        <?php if ($current_page > 1): ?>
+        <a href="index.php?page=products&p=<?=$current_page-1?>">Prev</a>
+        <?php endif; ?>
+        <?php if ($total_products > ($current_page * $num_products_on_each_page) - $num_products_on_each_page + count($products)): ?>
+        <a href="index.php?page=products&p=<?=$current_page+1?>">Next</a>
+        <?php endif; ?>
+    </div>
+</div>
+
+<?=template_footer()?>
